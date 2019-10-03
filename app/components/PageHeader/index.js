00a1d8482cf14ba6acfe75ed43813192707b80d9
Leroy Nas <leroynas119@gmail.com>
2019-10-03 17:50:25 +0200

    feat: added bottom border to PageHeader

--- app/components/PageHeader/index.js
+++ app/components/PageHeader/index.js
@@ -6,10 +6,21 @@
 
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
 import { CardHeader } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  header: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    borderBottomStyle: 'solid',
+  },
+});
+
 function PageHeader({ title }) {
-  return <CardHeader title={title} />;
+  const classes = useStyles();
+
+  return <CardHeader title={title} className={classes.header} />;
 }
 
 PageHeader.propTypes = {
