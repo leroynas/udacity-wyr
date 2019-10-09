5a64a8f0c8684a72fca12d9a77bce6349b7893d8
Leroy Nas <leroynas119@gmail.com>
2019-10-09 21:04:57 +0200

    feat: Header ui component

---
+++ app/components/ui/Heading/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Heading = styled.p`
+  font-family: ${props => props.theme.font.family.secondary};
+  font-size: ${props => props.theme.font.size[props.size]};
+  margin: 0;
+`;
+
+Heading.propTypes = {
+  size: PropTypes.string,
+};
+
+Heading.defaultProps = {
+  size: 'lg',
+};
+
+export default Heading;
