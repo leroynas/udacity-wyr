2be2021f0ab4203786a0ad1a7755f41abb2addd0
Leroy Nas <leroynas119@gmail.com>
2019-10-09 21:53:28 +0200

    fix: changed Heading element to span

--- app/components/ui/Heading/index.js
+++ app/components/ui/Heading/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Heading = styled.p`
+const Heading = styled.span`
   font-family: ${props => props.theme.font.family.secondary};
   font-size: ${props => props.theme.font.size[props.size]};
   margin: 0;
