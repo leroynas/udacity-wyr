56ddac20921f00b67b8716d5bcffc969977da6ff
Leroy Nas <leroynas119@gmail.com>
2019-10-09 20:40:52 +0200

    feat: added theme variables

--- app/core/theme.js
+++ app/core/theme.js
@@ -1,3 +1,28 @@
-const theme = {};
+const theme = {
+  font: {
+    family: {
+      primary: '"Source Sans Pro", calibri',
+      secondary: '"Create Round", droid-serif',
+    },
+    size: {
+      xl: '30px',
+      lg: '22px',
+      md: '16px',
+      sm: '15px',
+    },
+  },
+  palette: {
+    primary: {
+      main: '#3c78d8',
+      light: '#5985d5',
+      text: '#ffffff',
+    },
+    secondary: {},
+    default: {
+      main: '#ffffff',
+      text: '#000000',
+    },
+  },
+};
 
 export default theme;
